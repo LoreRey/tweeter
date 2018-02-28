459f573c29088b8e5ed8c15b0bf146f625d3f4a7
Lorena Reyes <lorena.n.reyes@gmail.com>
2018-02-28 02:20:37 +0000

    character counter done, and tweet set-up

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+ $(".new-tweet form textarea").on("keyup", function() {
+    const char  = $(this).val().length;
+    const max = 140;
+    const count = $('.counter');
+    count.text(max - char);
+    if(char > max) {
+      count.addClass("overMax");
+    } else {
+      count.removeClass("overMax");
+    }
+  });
+});
+
+//classof for css
+//sibling to get to counter this.sibling
+
