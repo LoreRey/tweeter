306230235a6659412272abba60279442cc26febc
Lorena Reyes <lorena.n.reyes@gmail.com>
2018-02-28 23:56:59 +0000

    refetching tweet on submission - security

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,52 +1,11 @@
 
- /*const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];*/
-
 $(document).ready(function() {
+
+function escape(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}
 
 function createTweetElement(tweets) {
   let tweetContent = tweets.content.text;
@@ -58,14 +17,14 @@ function createTweetElement(tweets) {
   return `
   <article>
   <header>
-    <img class="icon" src="${tweetAvatar}" />
-    <h2>${tweetUser}</h2>
-    <p class="user_handle">${tweetHandle}</p>
+    <img class="icon" src="${escape(tweetAvatar)}" />
+    <h2>${escape(tweetUser)}</h2>
+    <p class="user_handle">${escape(tweetHandle)}</p>
   </header>
-    <p class="tweet">${tweetContent}</p>
+    <p class="tweet">${escape(tweetContent)}</p>
   <footer class="footer">
     <div>
-      <p class="date">${tweetCreatedAt}</p>
+      <p class="date">${escape(tweetCreatedAt)}</p>
     </div>
     <div>
       <img class="bt_icons" src="/images/tweetretweet.png">
@@ -81,6 +40,7 @@ function renderTweets(tweets) {
   tweets.forEach(function(tweet) {
       let newTweet = createTweetElement(tweet);
       $(".tweets").prepend(newTweet);
+      console.log(newTweet)
   });
 }
 
