c0f0a8e60e737ec85b6d646639745f543255f857
Lorena Reyes <lorena.n.reyes@gmail.com>
2018-03-01 22:46:52 +0000

    added time stamp to tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -24,7 +24,7 @@ function createTweetElement(tweets) {
     <p class="tweet">${escape(tweetContent)}</p>
   <footer class="footer">
     <div>
-      <p class="date">${escape(tweetCreatedAt)}</p>
+      <p class="date">${escape(timeStamp(Date.now(), tweetCreatedAt))}</p>
     </div>
     <div>
       <img class="bt_icons" src="/images/tweetretweet.png">
@@ -87,10 +87,35 @@ function loadTweets() {
  loadTweets();
 
 
+
+function timeStamp(now, date) {
+let seconds = Math.floor(now - date) / 1000;
+let interval = Math.floor(seconds / 31536000);
+  if (interval > 1) {
+    return interval + " years ago";
+  }
+  interval = Math.floor(seconds / 2592000);
+  if (interval > 1) {
+    return interval + " months ago";
+  }
+  interval = Math.floor(seconds / 86400);
+  if (interval > 1) {
+    return interval + " days ago";
+  }
+  interval = Math.floor(seconds / 3600);
+  if (interval > 1) {
+    return interval + " hours ago";
+  }
+  interval = Math.floor(seconds / 60);
+  if (interval > 1) {
+    return interval + " minutes ago";
+  }
+  return Math.floor(seconds) + " seconds ago";
+}
+
+
+
 });
-
-
-
 
 
 
